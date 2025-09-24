663f82b629f95ae02396f7fc7358c068fb52ce22
Yuanqin Fan <fanyuanqin96@gmail.com>
2025-09-23 22:34:20 -0400

    Add Game Graveyard section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,12 @@
 'use client';
 import HeroGames from '@/components/pages/homepage/hero-games';
+import GameGraveyard from '@/components/pages/homepage/game-graveyard';
 
 const HomePage = () => {
   return (
     <div className="container mx-auto space-y-12 px-4 py-8">
       <HeroGames />
+      <GameGraveyard />
     </div>
   );
 };
